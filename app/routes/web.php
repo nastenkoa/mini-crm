1cdbda01da02032740338aac85de70e66cf96bca
Alexander Nastenko <nastenko.as@gmail.com>
2025-06-29 09:53:08 +0200

    feat: set up basic route structure with role-based separation

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,16 +1,28 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware(['auth', 'verified'])->prefix('client')->name('client.')->group(function () {
+    //Route::get('/dashboard', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('dashboard');
+    //Route::resource('orders', \App\Http\Controllers\Client\OrderController::class)->only(['index', 'create', 'store']);
+});
+
+Route::middleware(['auth', IsAdmin::class])->prefix('admin')->name('admin.')->group(function () {
+    //Route::get('/dashboard', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+    //Route::resource('orders', \App\Http\Controllers\Admin\OrderController::class);
+});
+
+/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+*/
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
